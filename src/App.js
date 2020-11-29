09b4f5d1e0d3efc642d6fb92dc2063bd2a5b1ab9
Sofia Mackinnon <sofi.mackinnon@gmail.com>
2020-11-29 12:49:54 -0300

    primer commit desafio clase 2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1>Cambalache Tiendita</h1>
+      <h2>Bienvenidxs! </h2>
+
+      <section>
+        <img src='img/vela1.jpeg'></img>
+        <img src='img/vela2.jpeg'></img>
+        <img src='img/vela3.jpeg'></img>
+      </section>
     </div>
   );
 }
